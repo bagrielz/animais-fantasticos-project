6f284a820ab0281427c6ee1b2b197d1fdeda0239
Gabriel Stênio <gabri.stnio50@hotmail.com>
2022-02-24 14:40:04 -0300

    Modal transformado em classe.

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,25 +1,44 @@
-const activeClass = "active";
-
 // Modal
-export default function initModal() {
-  const openButton = document.querySelector('[data-modal="open"');
-  const closeButton = document.querySelector('[data-modal="close"');
-  const containerModal = document.querySelector('[data-modal="container"');
-
-  function toggleModal(event) {
-    event.preventDefault();
-    containerModal.classList.toggle(activeClass);
+export default class Modal {
+  constructor(openButton, closeButton, containerModal) {
+    this.openButton = document.querySelector(openButton);
+    this.closeButton = document.querySelector(closeButton);
+    this.containerModal = document.querySelector(containerModal);
+    this.activeClass = "active";
+    // O método bind() muda a referência atual do this para a classe.
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.clickAwayModal = this.clickAwayModal.bind(this);
   }
 
-  function clickAwayModal(event) {
-    if (event.target === this) {
-      toggleModal(event);
+  // Abre ou fecha o modal.
+  classToggleModal() {
+    this.containerModal.classList.toggle(this.activeClass);
+  }
+
+  // Adiciona o evento de toggle ao modal.
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.classToggleModal();
+  }
+
+  // Fecha o modal ao clicar do lado de fora.
+  clickAwayModal(event) {
+    if (event.target === this.containerModal) {
+      this.classToggleModal();
     }
   }
 
-  if (openButton && closeButton && containerModal) {
-    openButton.addEventListener("click", toggleModal);
-    closeButton.addEventListener("click", toggleModal);
-    containerModal.addEventListener("click", clickAwayModal);
+  // Adiciona os eventos aos elementos do modal.
+  addModalEvents() {
+    this.openButton.addEventListener("click", this.eventToggleModal);
+    this.closeButton.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.clickAwayModal);
+  }
+
+  init() {
+    if (this.openButton && this.closeButton && this.containerModal) {
+      this.addModalEvents();
+    }
+    return this;
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import SmoothScroll from "./modules/smooth-scroll.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -13,16 +13,22 @@ import initScrollAnimation from "./modules/scroll-animation.js";
 const smoothScroll = new SmoothScroll('[data-menu="smooth"] a[href^="#"]');
 smoothScroll.init();
 
-const accordion = new Accordion("[data-anime='accordion'] dt");
+const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
 const tabNav = new TabNav(
-  "[data-tab='menu'] li",
-  "[data-tab='content'] section"
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
 );
 tabNav.init();
 
-initModal();
+const modal = new Modal(
+  '[data-modal="open"',
+  '[data-modal="close"',
+  '[data-modal="container"'
+);
+modal.init();
+
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
